cf5432b647521cb8f93b6f627eec34d3df10e58b
inurao <inurao@yahoo.com.br>
2025-08-01 20:51:46 -0300

    Update LoginServiceTest.java

--- src/test/java/auth/LoginServiceTest.java
+++ src/test/java/auth/LoginServiceTest.java
@@ -1,19 +1,31 @@
-// src/test/java/auth/LoginServiceTest.java
 package auth;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginServiceTest {
-    LoginService login = new LoginService();
+
+    private final LoginService login = new LoginService();
 
     @Test
     public void testLoginCorreto() {
-        assertTrue(login.autenticar("admin", "1234"));
+        assertTrue(login.autenticar("admin", "1234"), "Deve autenticar com usuário e senha corretos");
     }
 
-    // TODO: Testes que o aluno deve implementar:
-    // testLoginIncorreto
-    // testUsuarioNulo
-    // testSenhaNula
+    @Test
+    public void testLoginIncorretoSenha() {
+        assertFalse(login.autenticar("admin", "senhaErrada"), "Não deve autenticar com senha incorreta");
+    }
+
+    @Test
+    public void testLoginIncorretoUsuario() {
+        assertFalse(login.autenticar("usuarioErrado", "1234"), "Não deve autenticar com usuário incorreto");
+    }
+
+    @Test
+    public void testLoginNulo() {
+        assertFalse(login.autenticar(null, "1234"), "Não deve autenticar com usuário nulo");
+        assertFalse(login.autenticar("admin", null), "Não deve autenticar com senha nula");
+        assertFalse(login.autenticar(null, null), "Não deve autenticar com usuário e senha nulos");
+    }
 }
