29f9fbcf980b4c2d03998b76f7b88d5df01f156f
rodolfooliveira-Professor <rodolfooliveira@id.uff.br>
2025-08-03 18:14:12 -0300

    Update LoginServiceTest.java

--- src/test/java/auth/LoginServiceTest.java
+++ src/test/java/auth/LoginServiceTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginServiceTest {
     LoginService login = new LoginService();
 
-    @Test
-    public void testLoginCorreto() {
-        assertTrue(login.autenticar("admin", "1234"));
-    }
+    //@Test
+    //public void testLoginCorreto() {
+    //    assertTrue(login.autenticar("admin", "1234"));
+    //}
 
     // TODO: Testes que o aluno deve implementar:
     // testLoginIncorreto
